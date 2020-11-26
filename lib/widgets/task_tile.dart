c2a1d036798f39a4785ef0693519dc568831a1c3
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-26 21:43:16 +0600

    Working with ListView.builder

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  TaskTile({this.isChecked, this.title, this.onChanged});
+
+  final bool isChecked;
+  final String title;
+  final Function onChanged;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('asd'),
+      title: Text(
+        title,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: onChanged,
       ),
     );
   }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'buy milk'),
+    Task(name: 'buy eggs'),
+    Task(name: 'buy bread'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          title: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          onChanged: (newValue) {
+            setState(
+              () {
+                tasks[index].toggleDone();
+              },
+            );
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
