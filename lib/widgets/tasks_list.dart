a32c9ccc082ad346ff361152c8d2e745571b6162
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-28 16:03:06 +0600

    Refactoring

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/models/task_data.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
